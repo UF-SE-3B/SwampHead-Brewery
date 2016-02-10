a04f6cba7a48a8434b8f0eef3823bc8022a45458
Cole Burton <cole.l.burton@gmail.com>
2016-02-10 16:50:28 -0500

    added user role: bartender

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -79,9 +79,9 @@ var UserSchema = new Schema({
   roles: {
     type: [{
       type: String,
-      enum: ['user', 'admin']
+      enum: ['bartender', 'user', 'admin']
     }],
-    default: ['user'],
+    default: ['bartender'],
     required: 'Please provide at least one role'
   },
   updated: {
@@ -177,7 +177,7 @@ UserSchema.statics.generateRandomPassphrase = function () {
     var password = '';
     var repeatingCharacters = new RegExp('(.)\\1{2,}', 'g');
 
-    // iterate until the we have a valid passphrase. 
+    // iterate until the we have a valid passphrase.
     // NOTE: Should rarely iterate more than once, but we need this to ensure no repeating characters are present.
     while (password.length < 20 || repeatingCharacters.test(password)) {
       // build the random password
