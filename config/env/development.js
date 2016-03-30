038cfb9e611eee2bd8d550eb49e1fa6623b5bdd5
Javs Ramirez <jvramirez@users.noreply.github.com>
2016-03-30 10:08:33 -0400

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || ,
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI ,
     options: {
       user: '',
       pass: ''
