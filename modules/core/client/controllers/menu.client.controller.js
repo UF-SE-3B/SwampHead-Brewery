fd7c194f6c32f35f76870ec5c662bc530d5f2d2b
Collin Irwin <irwincollin+github@gmail.com>
2016-04-12 18:47:35 -0400

    Rudimentary drag and drop on menu page.

--- modules/core/client/controllers/menu.client.controller.js
+++ modules/core/client/controllers/menu.client.controller.js
@@ -9,13 +9,68 @@ angular.module('core').controller('MenuController', ['$scope', 'Authentication',
     var drinks = DrinksService.query();
     vm.drinks = drinks;
 
-    setInterval(function() {
+    $scope.reindexDrink = function(drink, index) {
+      if(drink.menuIndex !== index) {
+        drink.menuIndex = index;
+        drink.$update();
+      }
+    };
+
+    $scope.startDrinkDrag = function(event, ui, title) {
+      $scope.draggedIndex = this.$index;
+      $scope.draggedDrink = this.drink;
+    };
+
+    function updateDrinks() {
       var drinks = DrinksService.query();
       drinks.$promise.then(
         function(result) {
           vm.drinks = result;
         });
-    }, 5000);
+    }
+
+    function successCallback(res) {
+      updateDrinks();
+    }
+
+    function errorCallback(res) {
+      console.log("error callback");
+    }
+
+    $scope.dropOnDrink = function(event, ui) {
+      $scope.draggedDrink.menuIndex = this.$index;
+      this.drink.menuIndex = $scope.draggedIndex;
+      if($scope.draggedDrink.menuNumber === this.drink.menuNumber){
+        $scope.draggedDrink.$update(successCallback, errorCallback);
+        this.drink.$update(successCallback, errorCallback);
+      }
+    };
+
+    $scope.dropOnMenu0 = function(event, ui) {
+      if($scope.draggedDrink.menuNumber !== 0) {
+        $scope.draggedDrink.menuNumber = 0;
+
+        $scope.draggedDrink.$update(successCallback, errorCallback);
+      }
+    };
+
+    $scope.dropOnMenu1 = function(event, ui) {
+      if($scope.draggedDrink.menuNumber !== 1) {
+        $scope.draggedDrink.menuNumber = 1;
+
+        $scope.draggedDrink.$update(successCallback, errorCallback);
+      }
+    };
+
+    $scope.dropOnMenu2 = function(event, ui) {
+      if($scope.draggedDrink.menuNumber !== 2) {
+        $scope.draggedDrink.menuNumber = 2;
+
+        $scope.draggedDrink.$update(successCallback, errorCallback);
+      }
+    };
+
+    setInterval(updateDrinks(), 5000);
 
     $scope.topbarActive = true;
   }
