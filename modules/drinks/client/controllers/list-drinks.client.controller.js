870b08eac1b42ceadc2565de14239180a706bf79
Collin Irwin <irwincollin+github@gmail.com>
2016-04-22 13:31:01 -0400

    Switched the drag and drop from menu to drinks list

--- modules/drinks/client/controllers/list-drinks.client.controller.js
+++ modules/drinks/client/controllers/list-drinks.client.controller.js
@@ -42,5 +42,82 @@
       );
     }
 
+    var timer;
+
+    function updateDrinks() {
+      var drinks = DrinksService.query();
+      drinks.$promise.then(
+        function(result) {
+          vm.drinks = result;
+        });
+    }
+
+    function delayTimer() {
+      if(timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(updateDrinks, 500);
+    }
+
+    function successCallback(res) {
+      delayTimer();
+    }
+
+    function errorCallback(res) {
+      console.log("error callback");
+    }
+
+    function doDelayedUpdate(drink) {
+      delayTimer();
+      setTimeout(function() { doUpdate(drink); }, 100);
+    }
+
+    function doUpdate(drink) {
+      drink.$update(successCallback, errorCallback);
+    }
+
+    $scope.reindexDrink = function(drink, index) {
+      if(drink.menuIndex !== index) {
+        drink.menuIndex = index;
+        doUpdate(drink);
+      }
+    };
+
+    $scope.startDrinkDrag = function(event, ui, title) {
+      $scope.draggedIndex = this.$index;
+      $scope.draggedDrink = this.drink;
+    };
+
+    $scope.dropOnDrink = function(event, ui) {
+      $scope.draggedDrink.menuIndex = this.$index;
+      if($scope.draggedDrink.menuNumber === this.drink.menuNumber){
+        this.drink.menuIndex = $scope.draggedIndex;
+        doUpdate($scope.draggedDrink);
+        doUpdate(this.drink);
+      }
+    };
+
+    $scope.dropOnMenu0 = function(event, ui) {
+      if($scope.draggedDrink.menuNumber !== 0) {
+        $scope.draggedDrink.menuNumber = 0;
+        doDelayedUpdate($scope.draggedDrink);
+      }
+    };
+
+    $scope.dropOnMenu1 = function(event, ui) {
+      if($scope.draggedDrink.menuNumber !== 1) {
+        $scope.draggedDrink.menuNumber = 1;
+        doDelayedUpdate($scope.draggedDrink);
+      }
+    };
+
+    $scope.dropOnMenu2 = function(event, ui) {
+      if($scope.draggedDrink.menuNumber !== 2) {
+        $scope.draggedDrink.menuNumber = 2;
+        doDelayedUpdate($scope.draggedDrink);
+      }
+    };
+
+
   }
 })();
